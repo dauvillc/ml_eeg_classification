2af2cab2cbd83d452f9e66a90572bac3fffc0bab
emma-farina <91537819+emma-farina@users.noreply.github.com>
2021-11-29 14:47:42 +0100

    Update data_exploration.py

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -7,6 +7,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy as sp
+import cv2
 
 FIF_PATH = os.path.join('data', 'fif_data', 'offline', 'fif',
                         '20210812-113427-WS-default-raw.fif')
@@ -38,3 +40,52 @@ if __name__ == "__main__":
     raw.plot(events=events, start=0, duration=40, color='gray', scalings='auto',
              event_color={9: 'r', 10: 'g', 11: 'b', 12: 'm', 13: 'y', 14: 'k', 15:'c'})
     plt.show()
+    
+    
+    """ EMMA - I added the part you wrote in the notebook for epoching & retrieving groundtruth labels
+    and what I wrote, but I'm not sure it does what its supposed to do """
+    
+    # Bandpass filtering in frequency range 1-100 Hz
+    raw.filter(1, 100)
+
+    # Notch filtering
+    raw.notch_filter(50)
+
+    # Re-referencing to EEG average
+    raw.set_eeg_reference('average', projection=True)
+
+    # Plot pre-processed signals
+    raw.plot(events=events, start=0, duration=10, color='gray', scalings=1e2,
+             event_color={9: 'r', 10: 'g', 11: 'b', 12: 'm', 13: 'y', 14: 'k', 15: 'c'},
+             title='Preprocessed EEG signals')
+    plt.show()
+    
+    # Retrieves the timesteps at which a 14 event happens
+    epoch_starts = events[np.where(events[:, 2] == 14), 0][0]
+    # Same for event 13
+    epoch_ends = events[np.where(events[:, 2] == 13), 0][0]
+    # Extracts each epoch and puts them into a list using the previous arrays
+    epochs = [raw.get_data()[:, st:end + 1] for st, end in zip(epoch_starts, epoch_ends)]
+    # Cuts each epoch at the minimum length among all epochs
+    epochs_lengths = [ep.shape[1] for ep in epochs]
+    epochs = [ep[:, :min(epochs_lengths)] for ep in epochs]
+    # Assembles the epochs into an array of shape (n_epochs, n_channels, n_timesteps)
+    epochs = np.stack(epochs)
+
+    # Returns all events 12 or 10 that happened, in chronological order
+    label_events = events[np.where(np.isin(events[:, 2], [12, 10])), 2][0]
+    # Converts all the labels to 'ghi'=0 or 'fo'=1
+    epochs_true_labels = (label_events == 12).astype(int)
+    
+    # Transformation between electrode pairs for each epoch: abs(FFT(Ei)-FFT(Ej))
+    for z in range(epochs.shape[0]):
+        data = (abs(sp.ifft(epochs[z, 0, :]) - sp.ifft(epochs[z, 1, :]))).T
+        for i in range(1, epochs.shape[1]):
+            for j in range(i, epochs.shape[1]):
+                col = (abs(sp.ifft(epochs[z, i, :]) - sp.ifft(epochs[z, j, :]))).T
+                data = np.column_stack((data, col))
+        # Normalization between 0 and 1
+        data = (data - np.min(data)) / (np.max(data) - np.min(data))
+        # Saves images
+        IMG_PATH = os.path.join('SZ_dry_D3-speechmiAdaptive', 'offline', 'images')
+        cv2.imwrite(f"{IMG_PATH}\\image%d.jpg" % z, data)
